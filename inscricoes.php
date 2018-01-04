83239430875b59f8386667a2ce84a2b9a7e9cf7e
Marcio Yonamine <marcioyonamine@gmail.com>
2018-01-04 09:25:31 -0200

    merge

--- inscricoes.php
+++ inscricoes.php
@@ -3,7 +3,20 @@
   <body>
   
   <?php include "menu/menu_editais.php"; ?>
- 
+
+<?php 
+if(isset($_GET['p'])){
+	$p = $_GET['p'];
+}else{
+	$p = 'inicio';
+}
+
+
+switch($p){
+	case "inicio":
+?>
+
+  
         <main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
           <h1>Inscrições</h1>
 
@@ -75,7 +88,93 @@
 				</tbody>
             </table>
           </div>
-        </main>
+
+<?php 
+break;
+case 'all':
+
+?>
+        <main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
+          <h1>Inscrições</h1>
+
+
+          <p>Para ter acesso aos detalhes dos projetos, é necessário que esteja logado no CulturAZ e que faça parte da equipe de pareceristas.</p>
+		<!--<div><select>
+		<option></option>
+		<input class="btn btn-sm btn-default" type="submit" value="Filtrar" />
+		</select></div>-->
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+					<th>CulturAZ</th>
+					<th>Título</th>
+                    <th>Proponente</th>
+					<th>Cat</th>
+                  <th>Área</th>
+                  <th>Valor</th>
+                  <th>Nota</th>
+					<th></th>
+				  </tr>
+              </thead>
+              <tbody>
+				<?php 
+				global $wpdb;
+				$tipo = 'usuario';
+				$id = 1;
+				$x = opcaoDados($tipo,$id);
+				$g = $x['edital'][1];
+				
+				$edital =  editais("",19);
+
+				/*
+				$sql_sel_ins = "SELECT avaliadores FROM ava_edital WHERE id_mapas = '273'";
+				$sel = $wpdb->get_row($sql_sel_ins,ARRAY_A);
+
+				$res = json_decode($sel['avaliadores'],true);
+				$inscritos = $res[$g];
+				//var_dump($res);
+				*/
+				$sql_sel_ins = "SELECT inscricao FROM ava_inscricao";
+				$res = $wpdb->get_results($sql_sel_ins,ARRAY_A);
+				
+				
+				for($i = 0; $i < count($res[$g]); $i++){
+					$id_insc = $res[$i]['inscricao'];
+					$sel = "SELECT descricao,inscricao FROM ava_inscricao WHERE inscricao = '$id_insc'";	
+					$json = $wpdb->get_row($sel,ARRAY_A);	
+					$res_json = converterObjParaArray(json_decode(($json['descricao'])));
+
+
+				?>	
+    			 <tr>
+                  <td><a href="http://culturaz.santoandre.sp.gov.br/inscricao/<?php echo substr($json['inscricao'],3); ?>" target="_blank" ><?php echo $json['inscricao']; ?> </a></td>
+
+                  <td><?php echo $res_json['3.1 - Título']; ?></td>
+                  <td><?php echo $res_json['Agente responsável pela inscrição']; ?></td>
+				<td><?php echo str_replace("CATEGORIA","",$res_json['3.2 - Categoria']); ?></td>
+                  <td><?php echo $res_json['3.3 - Determine a área principal de enquadramento da proposta']; ?></td>
+                  <td><?php echo $res_json['3.11 - Valor (em Reais)']; ?></td>
+				  <td></td>
+                  <td>
+				  <form method="POST" action="avaliacao.php" class="form-horizontal" role="form">
+							<input type="hidden" name="carregar" value="<?php echo $json['inscricao']; ?>" />
+							<input type="submit" class="btn btn-theme btn-sm btn-block" value="Avaliar">
+							</form></td>
+					</tr>
+				<?php 
+
+				} ?>	
+
+
+				</tbody>
+            </table>
+          </div>
+<?php
+break;
+} //fim switch
+?>
+		  </main>
       </div>
     </div>
 	
