0bb233df11dff382690a516efa49d1dcf616bafa
Marcio Yonamine <marcioyonamine@gmail.com>
2018-01-03 22:05:08 -0200

    avaliacao

--- avaliacao.php
+++ avaliacao.php
@@ -2,14 +2,32 @@
 <?php 
 if(isset($_POST['gravar'])){
 	$inscricao = $_POST['carregar'];
-	$sql_verifica = "SELECT id FROM ava_nota WHERE inscricao = '$inscricao AND usuario = '".$user->ID."'";
+	$sql_verifica = "SELECT id FROM ava_nota WHERE inscricao = '$inscricao' AND usuario = '".$user->ID."'";
 	$res = $wpdb->get_results($sql_verifica,ARRAY_A);
+	$usuario = $user->ID;
 	if(count($res) > 0){ // existe, atualiza
-		
+	 	foreach($_POST as $key => $value){
+			if(is_numeric($value) OR $value == ""){
+				$sql_atualiza = "UPDATE ava_nota SET nota = '$value' WHERE usuario = '$usuario' AND criterio = '$key' AND inscricao = '$inscricao'";
+				$ins = $wpdb->query($sql_atualiza);
+				//echo $sql_atualiza;
+
+			}
+		}
+	
+	
+		$mensagem = "Notas lançadas";
 		
 	}else{ // não existe, atualiza
-	$sql = "INSERT INTO `ava_nota` (`id`, `usuario`, `inscricao`, `nota`, `criterio`) VALUES (NULL, '".$user->ID."', '".$inscricao."', '".$."', '')";
-		
+	 	foreach($_POST as $key => $value){
+			if((is_numeric($value) OR $value == "")){
+			$sql_insere = "INSERT INTO `ava_nota` (`id`, `usuario`, `inscricao`, `nota`, `criterio`) VALUES (NULL, '$usuario', '$inscricao', '$value', '$key');";
+				$ins = $wpdb->query($sql_insere);
+				//echo $sql_insere;
+			}
+		}
+
+		$mensagem = "Notas atualizadas";
 	}
 }
 ?>
@@ -22,7 +40,7 @@ if(isset($_POST['gravar'])){
  <script src="js/maskMoney.js"></script> 
  <script>
 $(function() {
-	$( ".nota" ).mask("9,9");
+	$( ".nota" ).mask("9.9");
 });
   </script>
   <?php include "menu/menu_editais.php"; ?>
@@ -30,7 +48,7 @@ $(function() {
         <main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
           <h1>Dashboard</h1>
 
-          <h2>Section title</h2>
+          <h2></h2>
           <div class="table-responsive">
             <table class="table table-striped">
               <thead>
@@ -49,7 +67,7 @@ $(function() {
 				?>	
                 <tr>
                   <td><?php echo $res[$i]['criterio']?></td>
-					<td><input type="text" class="form-control nota" name="<?php echo $res[$i]['id']; ?>"></td>
+					<td><input type="text" class="form-control nota" name="<?php echo $res[$i]['id']; ?>" value="<?php echo retornaNota($res[$i]['id'],$user->ID); ?>"></td>
 				  </tr>
 				  
 				<?php } ?>
@@ -64,7 +82,31 @@ $(function() {
         </main>
       </div>
     </div>
+	<div>
+	<?php 
+	if(isset($_POST['gravar'])){
+	$inscricao = $_POST['carregar'];
+	$sql_verifica = "SELECT id FROM ava_nota WHERE inscricao = '$inscricao AND usuario = '".$user->ID."'";
+	$res = $wpdb->get_results($sql_verifica,ARRAY_A);
+
+	foreach($_POST as $key => $value){
+		echo $value."<br />";
+		
+	}
 	
-<?php 
+	if(count($res) > 0){ // existe, atualiza
+
+	
+		$mensagem = "Notas lançadas";
+		
+	}else{ // não existe, atualiza
+	//$sql = "INSERT INTO `ava_nota` (`id`, `usuario`, `inscricao`, `nota`, `criterio`) VALUES (NULL, '".$user->ID."', '".$inscricao."', '".$."', '')";
+		$mensagem = "Notas atualizadas";
+	}
+}
+	
+	?>
+</div>
+	<?php 
 include "footer.php";
 ?>
