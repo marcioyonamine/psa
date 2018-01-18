1f63567745b6d73df5fa90a374fbb625d3f96bf9
Marcio Yonamine <marcioyonamine@gmail.com>
2018-01-18 06:09:44 -0200

    edital 2fase

--- print.php
+++ print.php
@@ -61,7 +61,7 @@ if(isset($_GET['usuario'])){
                   <td><?php echo $res_json['3.11 - Valor (em Reais)']; ?></td>
 				  <td><?php echo somaNotas($json['inscricao'],$_GET['usuario'],273); ?></td>
                   <td>
-				  <?php $anot = retornaAnotacao($id_insc,$_GET['usuario'],273); echo $anot['anotacao']; ?>
+				  <?php $anot = retornaAnotacao($json['inscricao'],$_GET['usuario'],273); echo $anot['anotacao']; ?>
 				  </td>
 					</tr>
 				<?php 
