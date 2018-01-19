2b5976d02dddb9010fa48d4a9c91a464e4df92b9
Marcio Yonamine <marcioyonamine@gmail.com>
2018-01-19 12:39:36 -0200

    print

--- menu/me_relatorio.php
+++ menu/me_relatorio.php
@@ -24,7 +24,10 @@
 			<li class="nav-item">
               <a class="nav-link" href="orcamentoprint.php" target="_blank">Orçamento para impressão</a>
             </li>
-		<!--
+			<li class="nav-item">
+              <a class="nav-link" href="relatorioaniversario.php" target="_blank">Relatório do Edital de Aniversário para Impressão</a>
+            </li>
+			<!--
 			<li class="nav-item">
               <a class="nav-link" href="contratacao.php?p=busca_pf">Insere Contratação PF</a>
             </li>
