d41648dc8cc827746b704e69ddaab50aee26f11f
Marcio Yonamine <marcioyonamine@gmail.com>
2018-05-13 18:40:04 -0300

    fip

--- inscricoes.php
+++ inscricoes.php
@@ -59,7 +59,7 @@ switch($p){
 				$edital =  editais($id,21);
 
 				
-				$sql_sel_ins = "SELECT avaliadores FROM ava_edital WHERE id_mapas = '".$_GET['id_mapas']."'";
+				$sql_sel_ins = "SELECT avaliadores FROM ava_edital WHERE id_mapas = '".$_GET['edital']."'";
 				$sel = $wpdb->get_row($sql_sel_ins,ARRAY_A);
 
 				$res = json_decode($sel['avaliadores'],true);
