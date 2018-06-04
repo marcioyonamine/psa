4000a3bdd306b0fe5b945f1346f93dee79699605
Marcio Yonamine <marcioyonamine@gmail.com>
2018-06-04 10:12:07 -0300

    fip2018

--- avaliacao2fase.php
+++ avaliacao2fase.php
@@ -173,7 +173,7 @@ var objeto = form1.obs.value
 				$note = $wpdb->get_results($sql_obs,ARRAY_A);
 				for($i = 0;$i < count($note); $i++){
 					$u = get_userdata($note[$i]['usuario']);
-					echo "<p>".$u->user_nicename.": ".$note[$i]['anotacao']."</p>";
+					echo "<p>".$u->first_name." ".$u->last_name.": ".$note[$i]['anotacao']."</p>";
 					
 				}
 				
