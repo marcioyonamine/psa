0624dd2f6518aeaa01dcd06b3004ff003d2d2dc8
Marcio Yonamine <marcioyonamine@gmail.com>
2018-06-12 17:40:44 -0300

    modelos fip2018

--- contratacao.php
+++ contratacao.php
@@ -1748,7 +1748,7 @@ break;
 					</div>
 					<div class="row">
 						<div class="col-12">
-					<a  class="btn btn-theme btn-lg btn-block" href="documentos.php?modelo=561&id=<?php echo $id_pedido?>" target="_blank">OS para pedido de contratação FIP 2018</a>
+					<a  class="btn btn-theme btn-lg btn-block" href="documentos.php?modelo=561&id=<?php echo $id_pedido?>" target="_blank">Caput / Gerencia de Compras 2018</a>
 						</div>
 					</div>						
 					<div class="row">
