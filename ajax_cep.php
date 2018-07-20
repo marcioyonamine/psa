f14a978bd03d82e3d94649ecbe7ba4b282d68579
Marcio Yonamine <marcioyonamine@gmail.com>
2018-07-20 12:49:29 -0300

    cep api

--- ajax_cep.php
+++ ajax_cep.php
@@ -7,6 +7,7 @@ if(isset($_GET['CEP'])){
 }else{
 	$cep = $_POST['CEP'];
 }
+/*
 $cep_index = substr($cep, 0, 5);
 $dados['sucesso'] = 0;
 $sql01 = "SELECT * FROM igsis_cep_cep_log_index WHERE cep5 = '$cep_index' LIMIT 0,1";
@@ -26,5 +27,18 @@ $dados['rua']     = $campo02['tp_logradouro']." ".$campo02['logradouro'];
 $dados['bairro']  = $campo02['bairro'];
 $dados['cidade']  = $campo02['cidade'];
 $dados['estado']  = strtoupper($campo01['uf']);
+ */
+	$get_addr = "https://viacep.com.br/ws/".$cep."/json/";
+ 	$ch = curl_init($get_addr);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$page = curl_exec($ch);
+	$campo02 = json_decode($page,true);
+	$dados['sucesso'] = 1;
+	$dados['rua']     = $campo02['logradouro'];
+	$dados['bairro']  = $campo02['bairro'];
+	$dados['cidade']  = $campo02['localidade'];
+	$dados['estado']  = strtoupper($campo02['uf']);
+ 
+ 
  
 echo json_encode($dados);
