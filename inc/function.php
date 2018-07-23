8373a6e831b1f9429e564b66fa7b32a663e8eceb
Marcio Yonamine <marcioyonamine@gmail.com>
2018-07-23 17:39:36 -0300

    branch v1

--- inc/function.php
+++ inc/function.php
@@ -1460,17 +1460,25 @@ function retornaLocais($idEvento){
 }
 
 function retornaCEP($cep){
-	$get_addr = "https://viacep.com.br/ws/".$cep."/json/";
- 	$ch = curl_init($get_addr);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	$page = curl_exec($ch);
-	$campo02 = json_decode($page,true);
+	global $wpdb;
+	$cep_index = substr($cep, 0, 5);
+	$dados['sucesso'] = 0;
+	$sql01 = "SELECT * FROM igsis_cep_cep_log_index WHERE cep5 = '$cep_index' LIMIT 0,1";
+	$campo01 = $wpdb->get_row($sql01,ARRAY_A);
+	$uf = "igsis_cep_".$campo01['uf'];
+	$sql02 = "SELECT * FROM $uf WHERE cep = '$cep'";
+	$campo02 = $wpdb->get_row($sql02,ARRAY_A);
+	$res = count($campo02);
+	 if($res > 0){
 	$dados['sucesso'] = 1;
-	$dados['rua']     = $campo02['logradouro'];
+	 }else{
+	$dados['sucesso'] = 0;
+	 }
+	$dados['rua']     = $campo02['tp_logradouro']." ".$campo02['logradouro'];
 	$dados['bairro']  = $campo02['bairro'];
-	$dados['cidade']  = $campo02['localidade'];
-	$dados['estado']  = strtoupper($campo02['uf']);
- 	return json_encode($dados);
+	$dados['cidade']  = $campo02['cidade'];
+	$dados['estado']  = strtoupper($campo01['uf']);
+	return $dados;
 }
 
 
