933c0aa29e46cbb262f8c2bba01951d5190979c5
Marcio Yonamine <marcioyonamine@gmail.com>
2018-10-10 10:48:19 -0300

    mercocidades

--- mercocidades.php
+++ mercocidades.php
@@ -2,7 +2,7 @@
 
   <body>
   
-  <?php include "menu/menu_editais.php"; ?>
+  <?php include "menu/me_mercocidades.php"; ?>
  
   <?php 
   
@@ -14,6 +14,24 @@
   
   switch($p){
   case 'inicio':
+  
+  function retornaDataAvaliacao($id_mercocidades,$user){
+	  global $wpdb;
+	  $sql = "SELECT data01,jurado01 FROM sc_mercocidades_nota WHERE id_mercocidades = '$id_mercocidades' AND id_usuario = '$user'";
+	  $x = $wpdb->get_row($sql,ARRAY_A);
+	  if($x == NULL){
+		  $y['data01'] = "N/A";
+		  $y['jurado01'] = "N/A";
+		  return $x;
+	  }else{
+		  $x['data01'] = exibirDataBr($x['data01']);
+		  return $x;
+	  }
+	  
+	  
+  }
+  
+  
   
   ?>
  
@@ -28,10 +46,12 @@
                 <tr>
                   <th>#</th>
                   <th>Título</th>
+                  <th>Categoria</th>
+                  <th>Inscrito</th>
+                  <th>Data</th>
+                  <th>Nota</th>
                   <th></th>
-                  <th></th>
-                  <th></th>
-                </tr>
+				  </tr>
               </thead>
               <tbody>
 
@@ -39,6 +59,7 @@
 			$sql_filmes = "SELECT * FROM sc_mercocidades";
 			$edital = $wpdb->get_results($sql_filmes,ARRAY_A);
 			for($i = 0; $i < count($edital); $i++){
+				$nota = (retornaDataAvaliacao($edital[$i]['id'],$user->ID)); 
 			?>		
 			  
 				<tr>
@@ -46,8 +67,10 @@
                   <td><?php echo $edital[$i]['COL27']; ?></td>
                   <td><?php echo $edital[$i]['COL32']; ?></td>
                   <td><?php echo $edital[$i]['COL2']; ?></td>
-                  <td>
-			<form action="?" method="GET" class="form-horizontal">
+				<td><?php echo $nota['data01']; ?></td>
+				<td><?php echo $nota['jurado01']; ?></td>
+				<td>
+			<form action="?p=view" method="POST" class="form-horizontal">
 			<input type="hidden" name="p" value="view">
 			<input type="hidden" name="id" value="<?php echo $edital[$i]['id']; ?>">
 			<input type="hidden" name="id" value="<?php echo $edital[$i]['id']; ?>">
@@ -61,11 +84,67 @@
 				</tbody>
             </table>
           </div>
+		  
+		  
 <?php 
 break;
 case 'view':
-$id = $_GET['id'];
+
+
+$id = $_POST['id'];
+$id_usuario = $user->ID;
 $sql = "SELECT * FROM sc_mercocidades WHERE id= '$id'";
+
+// verifica se existem as notas
+$sql_ver = "SELECT * FROM sc_mercocidades_nota WHERE id_mercocidades = '$id' AND id_usuario = '$id_usuario'";
+$ver = $wpdb->get_results($sql_ver,ARRAY_A);
+
+if(count($ver) == 0){ // atualiza
+	
+ // insere
+	$sql_ins = "INSERT INTO `sc_mercocidades_nota` (`id`, `id_usuario`, `id_mercocidades`, `um_video`, `quantos_videos`, `ano`, `classificado`, `categoria`, `minutagem`, `tema`, `legendagem`, `documentos`, `jurado01`, `data01`, `status`) VALUES (NULL, '$id_usuario', '$id', '', '', '', '', '', '', '', '', '', '', '', '')";
+	$ins = $wpdb->query($sql_ins);
+	$id = $wpdb->insert_id;
+}
+
+if(isset($_POST['salvar'])){
+  $um_video = $_POST["um_video"];
+  $quantos_videos = $_POST["quantos_videos"];
+  $ano = $_POST["ano"];
+  $categoria = $_POST["categoria"];
+  $minutagem = $_POST["minutagem"];
+  $tema = $_POST["tema"];
+  $legendagem = $_POST["legendagem"];
+  $jurado01 = $_POST["jurado01"];
+  $id_mercocidades = $_POST["id"];
+  $data01 = date('Y-m-d');
+  
+  $sql_upd = "UPDATE sc_mercocidades_nota SET
+  `um_video` = '$um_video',
+  `quantos_videos` = '$quantos_videos',
+  `ano` = '$ano',
+  `categoria` = '$categoria',
+  `minutagem` = '$minutagem',
+  `tema` = '$tema',
+  `legendagem` = '$tema',
+  `jurado01` = '$jurado01',
+  `data01` = '$data01'
+  WHERE id_mercocidades = '$id_mercocidades' AND id_usuario = '$id_usuario';
+  
+  
+  ";
+  $e = $wpdb->query($sql_upd);
+  if($e == 1){
+	  $mensagem = alerta("Avaliação atualizada","success");
+  }else{
+	  $mensagem = alerta("Erro ao atualizar","warning");
+  }
+  
+}
+
+
+
+
 $filme = $wpdb->get_row($sql,ARRAY_A);
 
 ?>	
@@ -73,7 +152,7 @@ $filme = $wpdb->get_row($sql,ARRAY_A);
 	
         <main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
 	<h1><?php echo $filme['COL27']; ?></h1>		
-	
+	<p><?php if(isset($mensagem)){echo $mensagem; }?></p>
 						 <div class="container">
         <div class="row">    
 				<div class="col-md-offset-2 col-md-8">
@@ -94,6 +173,107 @@ $filme = $wpdb->get_row($sql,ARRAY_A);
 				</div>
         </div>
 		</div>
+		          <div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+			  <?php 
+			  //recupera as notas
+			  $notas = $wpdb->get_row($sql_ver,ARRAY_A);
+			//var_dump($notas);	
+			  
+			  ?>
+			  
+			  
+			  <form method="POST" action="?p=view" class="form-horizontal" role="form">
+              <tbody>
+				<tr>
+				<td>Enviou apenas um vídeo?</td>
+				<td>
+					<select class="form-control" name="um_video" id="programa" >
+						<option value='1' <?php if($notas['um_video'] == 1){ echo " selected ";} ?> >Sim</option>
+						<option value='0' <?php if($notas['um_video'] == 0){ echo " selected ";} ?>>Não</option>
+					</select>
+				</td>
+				</tr>
+	
+				<tr>
+				<td>Se não, quantos vídeos enviou?</td>
+				<td>
+				<input type="text" name="quantos_videos" class="form-control" id="inputSubject" value="<?php echo $notas['quantos_videos'];?>"/>
+				</td>
+				</tr>
+				<tr>
+				<td>O vídeo foi feito em:</td>
+				<td>
+					<select class="form-control" name="ano" id="programa" >
+						<option value='2017' <?php if($notas['ano'] == '2017'){ echo " selected ";} ?>>2017</option>
+						<option value='2018' <?php if($notas['ano'] == '2018'){ echo " selected ";} ?>>2018</option>
+					</select>
+				</td>
+				</tr>
+				<tr>
+				<td>Vídeo se enquadra na categoria inscrita?</td>
+				<td>
+					<select class="form-control" name="categoria" id="programa" >
+						<option value='1'<?php if($notas['categoria'] == 1){ echo " selected ";} ?> >Sim</option>
+						<option value='0' <?php if($notas['categoria'] == 0){ echo " selected ";} ?>>Não</option>
+					</select>
+				</td>
+				</tr>
+				<tr>
+				<td>O vídeo está dentro da minutagem exigida?</td>
+				<td>
+					<select class="form-control" name="minutagem" id="programa" >
+						<option value='1'<?php if($notas['minutagem'] == 1){ echo " selected ";} ?> >Sim</option>
+						<option value='0' <?php if($notas['minutagem'] == 0){ echo " selected ";} ?>>Não</option>
+					</select>
+				</td>
+				</tr>
+				<tr>
+				<td>O vídeo se adequa ao tema?</td>
+				<td>
+					<select class="form-control" name="tema" id="programa" >
+						<option value='1'<?php if($notas['tema'] == 1){ echo " selected ";} ?> >Sim</option>
+						<option value='0' <?php if($notas['tema'] == 0){ echo " selected ";} ?>>Não</option>					</select>
+				</td>
+				</tr>				
+				<tr>
+				<td>A legedagem está correta?</td>
+				<td>
+					<select class="form-control" name="legendagem" id="programa" >
+						<option value='1'<?php if($notas['legendagem'] == 1){ echo " selected ";} ?> >Sim</option>
+						<option value='0' <?php if($notas['legendagem'] == 0){ echo " selected ";} ?>>Não</option>					</select>
+				</td>
+				</tr>				
+				<tr>
+				<td>Nota</td>
+				<td>
+					<select class="form-control" name="jurado01" id="programa" >
+						<option value='0' <?php if($notas['jurado01'] == 0){ echo " selected ";} ?>>0</option>
+						<option value='4' <?php if($notas['jurado01'] == 4){ echo " selected ";} ?> >4</option>
+						<option value='10' <?php if($notas['jurado01'] == 10){ echo " selected ";} ?>>10</option>
+
+					</select>
+						</td>
+				</tr>	
+								<tr>
+				<td>
+				<input type='hidden' name='id' value='<?php echo $notas['id_mercocidades'] ?>' >
+
+				<input type="submit" class="btn btn-theme btn-lg btn-block" name = 'salvar' value="Salvar">
+</td>
+				<td>
+				</td>
+				</tr>
+				
+				</tbody>
+            </table>
+          </div>
 	
 <?php 
 break;
