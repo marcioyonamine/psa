121e7dce1c34da4cd47e5735a2eabecc76d247ab
Marcio Yonamine <marcioyonamine@gmail.com>
2018-10-30 17:04:34 -0300

    fix relatorio

--- inc/function.php
+++ inc/function.php
@@ -2485,7 +2485,7 @@ function producao($idEvento){
 function somaPrograma($id){
 	global $wpdb;
 	$total = 0;
-	$sql_evento = "SELECT valor FROM sc_contratacao WHERE idEvento IN(SELECT idEvento FROM sc_evento WHERE idPrograma = '$id' AND publicado = '1' AND dataEnvio <> NULL AND cancelamento = 0) AND nLiberacao <> '' AND publicado ='1'";
+	$sql_evento = "SELECT valor FROM sc_contratacao WHERE idEvento IN(SELECT idEvento FROM sc_evento WHERE idPrograma = '$id' AND publicado = '1' AND dataEnvio IS NOT NULL AND cancelamento = 0) AND nLiberacao <> '' AND publicado ='1'";
 	$evento = $wpdb->get_results($sql_evento,ARRAY_A);
 
 	for($i = 0; $i < count($evento); $i++){
@@ -2512,7 +2512,7 @@ function somaPrograma($id){
 function somaProjeto($id){
 		global $wpdb;
 	$total = 0;
-	$sql_evento = "SELECT valor FROM sc_contratacao WHERE idEvento IN(SELECT idEvento FROM sc_evento WHERE idProjeto = '$id' AND publicado = '1') AND nLiberacao <> ''";
+	$sql_evento = "SELECT valor FROM sc_contratacao WHERE idEvento IN(SELECT idEvento FROM sc_evento WHERE idProjeto = '$id' AND publicado = '1' AND cancelamento = 0) AND nLiberacao <> ''";
 	$evento = $wpdb->get_results($sql_evento,ARRAY_A);
 
 	for($i = 0; $i < count($evento); $i++){
