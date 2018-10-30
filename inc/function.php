40a291107287152b5ea57619adaa5478e36188c1
Marcio Yonamine <marcioyonamine@gmail.com>
2018-10-30 17:07:09 -0300

    fix relatorio

--- inc/function.php
+++ inc/function.php
@@ -2512,7 +2512,7 @@ function somaPrograma($id){
 function somaProjeto($id){
 		global $wpdb;
 	$total = 0;
-	$sql_evento = "SELECT valor FROM sc_contratacao WHERE idEvento IN(SELECT idEvento FROM sc_evento WHERE idProjeto = '$id' AND publicado = '1' AND cancelamento = 0) AND nLiberacao <> ''";
+	$sql_evento = "SELECT valor FROM sc_contratacao WHERE idEvento IN(SELECT idEvento FROM sc_evento WHERE idProjeto = '$id' AND publicado = '1' AND cancelamento = 0 AND dataEnvio IS NOT NULL) AND nLiberacao <> ''";
 	$evento = $wpdb->get_results($sql_evento,ARRAY_A);
 
 	for($i = 0; $i < count($evento); $i++){
