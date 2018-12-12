feed5dfe26e0985b702ea4c907fb8885f16f1f75
Marcio Yonamine <marcioyonamine@gmail.com>
2018-12-12 11:23:51 -0200

    ranking territorios

--- ranking.php
+++ ranking.php
@@ -152,8 +152,8 @@ $(function() {
 
                   <td><?php echo $res_json['Agente responsável pela inscrição - Nome completo ou Razão Social']; ?></td>
 				<td><?php echo $res_json['Título']; ?></td>
-				<td><?php if(isset($nota['parecerista'][0]['nota'])){ echo $nota['parecerista'][0]['nota']; } ?></td>
-				<td><?php if(isset($nota['parecerista'][1]['nota'])){ echo $nota['parecerista'][1]['nota']; } ?></td>
+				<td><?php if(isset($nota['pareceristas'][0]['nota'])){ echo $nota['pareceristas'][0]['nota']; } ?></td>
+				<td><?php if(isset($nota['pareceristas'][1]['nota'])){ echo $nota['pareceristas'][1]['nota']; } ?></td>
 				<td><?php echo $res[$i]['nota']; //var_dump($nota);?></td>
                   <td>
 				 </td>
